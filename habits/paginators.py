bd1bfa1403251e49475586287c239b92e5220240
ElnaraGasanova <elnaraskypro@gmail.com>
2024-06-02 00:58:38 +0300

    У модели Habit оформлены Сериализаторы, Пагинация, Валидаторы.

--- habits/paginators.py
+++ habits/paginators.py
@@ -3,7 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class HabitPagination(PageNumberPagination):
     # Количество элементов на странице
-    page_size = 10
+    page_size = 5
     # Параметр запроса количества элементов на странице
     page_size_query_param = ('page_size')
     # Максимальное количество элементов на странице
