5dbd577c5e883024ae55353be99b09970edce2ec
ElnaraGasanova <elnaraskypro@gmail.com>
2024-06-03 20:37:57 +0300

    Внесены корректировки согласно рекомендациям Наставника, сообщения в ТГ так и не приходят.

--- habits/services.py
+++ habits/services.py
@@ -9,3 +9,8 @@ def send_tg_message(telegram_id, message):
     }
     response = requests.post(f'{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage', params=params)
     return response.json()
+
+    response_data = response.json()
+    if not response_data.get('ok'):
+        print(f"Ошибка отправки: {response_data}")
+    return response_data
--- habits/tasks.py
+++ habits/tasks.py
@@ -16,27 +16,31 @@ from habits.services import send_tg_message
 def send_tg_habits_reminder():
     '''ТГ-рассылка напоминаний о том, в какое время и какие
     привычки необходимо выполнять, если дата выполнения - сегодня.'''
-    today = timezone.now()       # .today().date отобразится в формате даты
+    today = timezone.now().date()     # .today().date отобразится в формате даты
     habits = Habit.objects.all()     # фильтруем привычки
-    message = f'Привет! Выполни {habits.action} {habits.time}, место - {habits.location}'
     # telegram_id_list = []     # выводим список привычек
 
     for habit in habits:
-        if habit.owner.telegram_id and habit.time >= today:
+        # Проверяем, что у пользователя есть telegram_id и время выполнения привычки соответствует сегодняшнему дню
+        if habit.owner.telegram_id and habit.time.date() == today:
+            # Формируем сообщение для текущей привычки
+            message = f'Привет! Выполни {habit.action} {habit.time}, место - {habit.location}'
             send_tg_message(habit.owner.telegram_id, message)
         # telegram_id_list.append(habit.owner.telegram_id)
+
+        # Обновление времени выполнения привычки в зависимости от periodicity
         if habit.periodicity == '1':
-            habit.time = today + timedelta(days=1)
+            habit.time += timedelta(days=1)
         elif habit.periodicity == '2':
-            habit.time = today + timedelta(days=2)
+            habit.time += timedelta(days=2)
         elif habit.periodicity == '3':
-            habit.time = today + timedelta(days=3)
-        elif habit.periodicity == '2':
-            habit.time = today + timedelta(days=4)
-        elif habit.periodicity == '2':
-            habit.time = today + timedelta(days=5)
-        elif habit.periodicity == '2':
-            habit.time = today + timedelta(days=6)
-        else:
-            habit.time = today + timedelta(days=7)
+            habit.time += timedelta(days=3)
+        elif habit.periodicity == '4':
+            habit.time += timedelta(days=4)
+        elif habit.periodicity == '5':
+            habit.time += timedelta(days=5)
+        elif habit.periodicity == '6':
+            habit.time += timedelta(days=6)
+        elif habit.periodicity == 'weekly':
+            habit.time += timedelta(days=7)
         habit.save()
